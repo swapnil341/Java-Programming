b5c4cc3c0a32f94019295c4f4b51eb888102ef60
swapnil341 <bhosaleswapnil341@gmail.com>
2025-06-24 12:24:48 +0530

    Fibonacci Series

---
+++ fibonacciSeries.java
@@ -0,0 +1,19 @@
+public class fibonacciSeries {
+	public static void main(String[] args) {
+		System.out.println("1\n1");
+		int a , b ;
+		a = b = 1 ;
+		int sum = 0 ;
+		int n = 10 ; 
+		while(n > 0) {
+			sum = a + b ;
+			System.out.println(sum);
+			b = a ;
+			a = sum ;
+			n-- ;
+
+		}
+
+	}
+
+}
