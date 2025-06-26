c333509d97f85421f6516dfbfa1244f2070c3f43
swapnil341 <bhosaleswapnil341@gmail.com>
2025-06-25 23:30:48 -0700

    Constructor Chaining

---
+++ constructorChaining.java
@@ -0,0 +1,41 @@
+// ******************** constructor chaining **************************** 
+class Person {
+	public String name;
+	public int id;
+
+	// parameter-less constructor
+	public Person() {
+		this("nan", 0);
+	}
+
+	// parameterized constructor
+	public Person(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public void setPerson(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public void getPerson() {
+		System.out.println("Name: " + name + "\nId: " + id);
+		System.out.println();
+	}
+}
+
+public class constructorChaining {
+
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setPerson("Abc", 100);
+		p.getPerson();
+		Person p2 = new Person("Xyz", 200);
+		p2.getPerson();
+		Person p3 = new Person();
+		p3.getPerson();
+
+	}
+
+}
